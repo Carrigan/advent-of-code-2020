6a76a7c0d9bec85bec2b5e31bf57a41edda7c169
Brian Carrigan <brian@bcarrigan.com>
2020-12-01 08:06:55 -0500

    Improve function signatures

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-fn find_sum_of_two(sum: i32, numbers: &[i32]) -> Option<(i32, i32)> {
+fn find_sum_of_two(numbers: &[i32], sum: i32) -> Option<(i32, i32)> {
     if numbers.len() <= 1 { return None; }
 
     let n1 = numbers[0];
@@ -9,22 +9,21 @@ fn find_sum_of_two(sum: i32, numbers: &[i32]) -> Option<(i32, i32)> {
         if n1 + n2 == sum { return Some((n1, *n2)) };
     }
 
-    find_sum_of_two(sum, &numbers[1..])
+    find_sum_of_two(&numbers[1..], sum)
 }
 
-fn find_sum_of_three(numbers: &[i32]) -> Option<(i32, i32, i32)> {
+fn find_sum_of_three(numbers: &[i32], sum: i32) -> Option<(i32, i32, i32)> {
     if numbers.len() <= 2 { return None; }
 
     let n1 = numbers[0];
 
-    match find_sum_of_two(2020 - n1, &numbers[1..]) {
+    match find_sum_of_two(&numbers[1..], sum - n1) {
         Some((n2, n3)) => Some((n1, n2, n3)),
-        None => find_sum_of_three(&numbers[1..])
+        None => find_sum_of_three(&numbers[1..], sum)
     }
 }
 
 fn main() {
-    
     let mut numbers: Vec<i32> = Vec::new();
 
     let input = fs::read_to_string("input").unwrap();
@@ -33,12 +32,12 @@ fn main() {
     }
 
     // Part 1
-    if let Some((n1, n2)) = find_sum_of_two(2020, numbers.as_slice()) {
+    if let Some((n1, n2)) = find_sum_of_two(numbers.as_slice(), 2020) {
         println!("{} * {} = {}", n1, n2, n1 * n2);
     }
 
     // Part 2
-    if let Some((n1, n2, n3)) = find_sum_of_three(numbers.as_slice()) {
+    if let Some((n1, n2, n3)) = find_sum_of_three(numbers.as_slice(), 2020) {
         println!("{} * {} * {} = {}", n1, n2, n3, n1 * n2 * n3);
     }
 }
@@ -46,7 +45,7 @@ fn main() {
 #[test]
 fn test_sum_two() {
     let numbers = [1721, 979, 366, 299, 675, 1456];
-    let (n1, n2) = find_sum_of_two(2020, &numbers).unwrap();
+    let (n1, n2) = find_sum_of_two(&numbers, 2020).unwrap();
 
     assert_eq!(n1 * n2, 514579);
 }
@@ -54,7 +53,7 @@ fn test_sum_two() {
 #[test]
 fn test_sum_three() {
     let numbers = [1721, 979, 366, 299, 675, 1456];
-    let (n1, n2, n3) = find_sum_of_three(&numbers).unwrap();
+    let (n1, n2, n3) = find_sum_of_three(&numbers, 2020).unwrap();
 
     assert_eq!(n1 * n2 * n3, 241861950);
 }
