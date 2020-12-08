867a4ab0a7fe0d867f9bf55190ab2fe060eb6e7f
Brian Carrigan <brian@bcarrigan.com>
2020-12-08 06:52:10 -0500

    Day 8, Part 2

--- day-8/src/main.rs
+++ day-8/src/main.rs
@@ -24,10 +24,12 @@ struct Program {
     pc: usize
 }
 
+#[derive(Debug, Eq, PartialEq)]
 enum StepResult {
     Ok,
     EndOfProgram,
-    AlreadyVisited
+    AlreadyVisited,
+    OutOfBounds
 }
 
 impl Program {
@@ -41,12 +43,21 @@ impl Program {
         Program { instructions, executed: Vec::new(), acc: 0, pc: 0 }
     }
 
-    fn run(&mut self) {
+    fn reset(&mut self) {
+        self.executed = Vec::new();
+        self.acc = 0;
+        self.pc = 0;
+    }
+
+    fn run(&mut self) -> Result<(), StepResult> {
+        self.reset();
+
         loop {
             match self.step() {
                 StepResult::Ok => (),
-                StepResult::EndOfProgram => panic!(),
-                StepResult::AlreadyVisited => break
+                StepResult::EndOfProgram => return Ok(()),
+                StepResult::AlreadyVisited => return Err(StepResult::AlreadyVisited),
+                StepResult::OutOfBounds => return Err(StepResult::OutOfBounds)
             }
         }
     }
@@ -63,19 +74,48 @@ impl Program {
             Instruction::Jmp(x) => ((self.pc as i32) + x) as usize
         };
 
+        if self.pc > self.instructions.len() {
+            return StepResult::OutOfBounds;
+        }
+
         if self.pc >= self.instructions.len() {
             return StepResult::EndOfProgram;
         }
 
         StepResult::Ok
     }
+
+    fn attempt_correction(&mut self) -> bool {
+        for index in 0..self.instructions.len() {
+            let (original_instruction, mutated_instruction) = match self.instructions[index] {
+                Instruction::Jmp(x) => (Instruction::Jmp(x), Instruction::Nop(x)),
+                Instruction::Nop(x) => (Instruction::Nop(x), Instruction::Jmp(x)), 
+                Instruction::Acc(_) => continue
+            };
+    
+            // Perform the swap
+            self.instructions[index] = mutated_instruction;
+    
+            // Run the program
+            match self.run() {
+                Ok(_) => return true,
+                Err(_) => self.instructions[index] = original_instruction
+            }
+        }
+
+        false
+    }
 }
 
 fn main() {
     // Part one
     let mut program = Program::new("input.txt");
-    program.run();
+    let _result = program.run();
     println!("Part 1: {}", program.acc);
+
+    // Part two
+    assert!(program.attempt_correction());
+    println!("Part 2: {}", program.acc);
 }
 
 #[test]
@@ -88,6 +128,17 @@ fn test_instruction_parser() {
 #[test]
 fn test_example_1() {
     let mut program = Program::new("example1.txt");
-    program.run();
+    let result = program.run();
+
+    assert_eq!(result.err().unwrap(), StepResult::AlreadyVisited);
     assert_eq!(program.acc, 5);
+}
+
+#[test]
+fn test_example_2() {
+    let mut program = Program::new("example1.txt");
+    let corrected = program.attempt_correction();
+
+    assert_eq!(corrected, true);
+    assert_eq!(program.acc, 8);
 }
