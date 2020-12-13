5e85c244aebcec0c6aaed37105705c23bc9f4d88
Brian Carrigan <brian@bcarrigan.com>
2020-12-13 10:01:33 -0500

    Day 13, Part 2

--- day-13/src/main.rs
+++ day-13/src/main.rs
@@ -1,3 +1,43 @@
+#[derive(Debug, Copy, Clone)]
+struct PeriodicWithOffsets {
+    period: u64,
+    offset_from_sync: u64,
+    first_sync: u64
+}
+
+impl PeriodicWithOffsets {
+    fn find_first_sync(&self, other: &PeriodicWithOffsets) -> u64 {
+        let mut x1 = self.first_sync;
+        let mut progress = 0;
+
+        while x1 < self.offset_from_sync { x1 += self.period; }
+
+        loop {
+            x1 += self.period;
+            progress += 1;
+
+            if progress == 100_000 {
+                print!(".");
+                progress = 0;
+            }
+
+            let relative_x1 = x1 + other.offset_from_sync - self.offset_from_sync;
+
+            if (relative_x1 >= other.first_sync) && (relative_x1 - other.first_sync) % other.period == 0 {
+                println!("{} {} {} {} {}", self.offset_from_sync, self.period, other.offset_from_sync, other.period, x1);
+                return x1 - self.offset_from_sync;
+            }
+        }
+    }
+
+    fn reduce(&self, other: &PeriodicWithOffsets) -> PeriodicWithOffsets {
+        let lcm = num::integer::lcm(self.period, other.period);
+        let offset = self.find_first_sync(other);
+
+        PeriodicWithOffsets { period: lcm, offset_from_sync: 0, first_sync: offset }
+    }
+}
+
 fn parse_input_part_one(path: &str) -> (u32, Vec<u32>) {
     let input = std::fs::read_to_string(path).unwrap();
     let mut lines = input.lines();
@@ -13,7 +53,7 @@ fn parse_input_part_one(path: &str) -> (u32, Vec<u32>) {
     (timestamp, buses)
 }
 
-fn parse_input_part_two(path: &str) -> Vec<(u32, u32)> {
+fn parse_input_part_two(path: &str) -> Vec<PeriodicWithOffsets> {
     let input = std::fs::read_to_string(path).unwrap();
     let mut lines = input.lines();
     lines.next();
@@ -24,7 +64,7 @@ fn parse_input_part_two(path: &str) -> Vec<(u32, u32)> {
         .split(',')
         .enumerate()
         .filter(|(_, bus)| *bus != "x")
-        .map(|(x, bus)| (x as u32, bus.parse().unwrap()))
+        .map(|(x, bus)| PeriodicWithOffsets { first_sync: 0, offset_from_sync: x as u64, period: bus.parse().unwrap() })
         .collect()
 }
 
@@ -36,17 +76,24 @@ fn soonest_arriving_after(timestamp: u32, buses: &Vec<u32>) -> (u32, u32) {
         .unwrap()
 }
 
-fn find_magic_time(buses: &Vec<(u32, u32)>) -> u64 {
-    let (step_offset, step) = buses.iter().max_by_key(|(_, s)| *s).unwrap();
-    let mut t: u64 = *step as u64 - *step_offset as u64;
+fn find_magic_time(periodics: &Vec<PeriodicWithOffsets>) -> u64 {
+    let mut reduced = periodics.clone();
 
-    loop {
-        if buses.iter().all(|(offset, bus)| (t + *offset as u64) % *bus as u64 == 0) {
-            return t;
-        }
+    while reduced.len() > 1 {
+        println!("{:?}", reduced);
 
-        t += *step as u64;
+        reduced = reduced
+            .chunks(2)
+            .map(|periodic_slice|
+                match periodic_slice {
+                    &[a, b] => a.reduce(&b),
+                    &[a] => a,
+                    _ => panic!()
+                })
+            .collect();
     }
+
+    reduced[0].first_sync
 }
 
 fn main() {
@@ -72,4 +119,13 @@ fn test_part_one() {
 fn test_part_two() {
     let buses = parse_input_part_two("example.txt");
     assert_eq!(find_magic_time(&buses), 1068781);
+
+    let buses = parse_input_part_two("example2.txt");
+    assert_eq!(find_magic_time(&buses), 3417);
+
+    let buses = parse_input_part_two("example3.txt");
+    assert_eq!(find_magic_time(&buses), 1202161486);
+
+    let buses = parse_input_part_two("example4.txt");
+    assert_eq!(find_magic_time(&buses), 1261476);
 }
