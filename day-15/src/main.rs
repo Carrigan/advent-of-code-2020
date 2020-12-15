23c2028699f957afe6f2251edd486ef8362046f4
Brian Carrigan <brian@bcarrigan.com>
2020-12-15 07:50:25 -0500

    Day 15, Part 2

--- day-15/src/main.rs
+++ day-15/src/main.rs
@@ -1,23 +1,32 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
 struct MemoryGame {
-    numbers: Vec<u32>
+    index: usize,
+    last_number: usize,
+    last_indeces: HashMap<usize, usize>
 }
 
 impl MemoryGame {
-    fn new(starters: &[u32]) -> Self {
-        Self { numbers: starters.iter().map(|n| *n).collect() }
+    fn new(starters: &[usize]) -> Self {
+        let mut last_indeces = HashMap::new();
+        for (i, &n) in starters[..starters.len() - 1].iter().enumerate() {
+            last_indeces.insert(n, i);
+        }
+
+        Self { index: starters.len() - 1, last_indeces, last_number: *starters.last().unwrap() }
     }
 
     fn generate(&mut self, size: usize) {
-        while self.numbers.len() < size {
-            let (rest, final_number) = self.numbers.split_at(self.numbers.len() - 1);
-            let last_heard_index = rest.iter().rev().position(|&x| x == final_number[0]);
-
-            let next_number = match last_heard_index {
-                Some(index) => index as u32 + 1,
+        while self.index < size - 1 {
+            let next_number = match self.last_indeces.get(&self.last_number) {
+                Some(index) => self.index - index,
                 None => 0
             };
 
-            self.numbers.push(next_number);
+            self.last_indeces.insert(self.last_number, self.index);
+            self.index += 1;
+            self.last_number = next_number;
         }
     }
 
@@ -27,13 +36,20 @@ fn main() {
     // Part one
     let mut game = MemoryGame::new(&[17, 1, 3, 16, 19, 0]);
     game.generate(2020);
-    println!("Part one: {}", *game.numbers.last().unwrap());
+    println!("Part one: {}", game.last_number);
+
+    // Part one
+    game.generate(30000000);
+    println!("Part two: {}", game.last_number);
 }
 
 #[test]
 fn test_part_one() {
     let mut game = MemoryGame::new(&[0, 3, 6]);
-    game.generate(2020);
 
-    assert_eq!(*game.numbers.last().unwrap(), 436);
+    game.generate(2020);
+    assert_eq!(game.last_number, 436);
+
+    game.generate(30000000);
+    assert_eq!(game.last_number, 175594);
 }
