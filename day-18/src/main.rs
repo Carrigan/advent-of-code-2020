0cf82c9f842638bf5365cad8dbb8075e9bfc3d1c
Brian Carrigan <brian@bcarrigan.com>
2020-12-18 07:11:20 -0500

    Day 18, Part 2

--- day-18/src/main.rs
+++ day-18/src/main.rs
@@ -26,7 +26,7 @@ fn apply(accumulator: u64, operand: u64, operator: Option<Token>) -> u64 {
     }
 }
 
-fn evaluate(tokens: &[Token]) -> (usize, u64) {
+fn evaluate_ltr(tokens: &[Token]) -> (usize, u64) {
     let mut total = 0;
     let mut index = 0;
     let mut context = Some(Token::AdditionSymbol);
@@ -43,7 +43,42 @@ fn evaluate(tokens: &[Token]) -> (usize, u64) {
                 context = None;
             }
             Token::ExpressionStart => {
-                let (increment, n) = evaluate(&tokens[index + 1..]);
+                let (increment, n) = evaluate_ltr(&tokens[index + 1..]);
+                total = apply(total, n, context);
+                context = None;
+                index += increment;
+            }
+            Token::ExpressionEnd => return (index + 1, total)
+        }
+
+        index += 1;
+    }
+
+    (index, total)
+}
+
+
+fn evaluate_with_precedence(tokens: &[Token]) -> (usize, u64) {
+    let mut total = 0;
+    let mut index = 0;
+    let mut context = Some(Token::AdditionSymbol);
+
+    while index < tokens.len() {
+        let token = &tokens[index];
+
+        match token {
+            // If mult/add: set context
+            Token::AdditionSymbol => context = Some(Token::AdditionSymbol),
+            Token::MultiplicationSymbol => {
+                let (increment, n) = evaluate_with_precedence(&tokens[index + 1..]);
+                return (index + increment + 1, total * n);
+            }
+            Token::Number(n) => {
+                total = apply(total, *n, context);
+                context = None;
+            }
+            Token::ExpressionStart => {
+                let (increment, n) = evaluate_with_precedence(&tokens[index + 1..]);
                 total = apply(total, n, context);
                 context = None;
                 index += increment;
@@ -62,25 +97,50 @@ fn main() {
     let sum: u64 = std::fs::read_to_string("input.txt").unwrap().lines()
         .map(|line| {
             let tokens = tokenize(line);
-            evaluate(&tokens).1
+            evaluate_ltr(&tokens).1
         })
         .sum();
 
     println!("Part one: {}", sum);
+
+    // Part Two
+    let sum: u64 = std::fs::read_to_string("input.txt").unwrap().lines()
+        .map(|line| {
+            let tokens = tokenize(line);
+            evaluate_with_precedence(&tokens).1
+        })
+        .sum();
+
+    println!("Part two: {}", sum);
 }
 
 #[test]
-fn test_tokenize() {
+fn test_evaluate_ltr() {
     let tokens = tokenize("2 * 3 + (4 * 5)");
-    assert_eq!(evaluate(&tokens).1, 26);
+    assert_eq!(evaluate_ltr(&tokens).1, 26);
 
     let tokens = tokenize("5 + (8 * 3 + 9 + 3 * 4 * 3)");
-    assert_eq!(evaluate(&tokens).1, 437);
+    assert_eq!(evaluate_ltr(&tokens).1, 437);
 
     let tokens = tokenize("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))");
-    assert_eq!(evaluate(&tokens).1, 12240);
+    assert_eq!(evaluate_ltr(&tokens).1, 12240);
 
     println!("last one");
     let tokens = tokenize("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2");
-    assert_eq!(evaluate(&tokens).1, 13632);
+    assert_eq!(evaluate_ltr(&tokens).1, 13632);
+}
+
+#[test]
+fn test_evaluate_with_predence() {
+    let tokens = tokenize("2 * 3 + (4 * 5)");
+    assert_eq!(evaluate_with_precedence(&tokens).1, 46);
+
+    let tokens = tokenize("5 + (8 * 3 + 9 + 3 * 4 * 3)");
+    assert_eq!(evaluate_with_precedence(&tokens).1, 1445);
+
+    let tokens = tokenize("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))");
+    assert_eq!(evaluate_with_precedence(&tokens).1, 669060);
+
+    let tokens = tokenize("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2");
+    assert_eq!(evaluate_with_precedence(&tokens).1, 23340);
 }
