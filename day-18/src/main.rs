3f23e11921c1d6a417d89df1166e9a3d7d2bf966
Brian Carrigan <brian@bcarrigan.com>
2020-12-18 06:52:59 -0500

    Day 18, Part 1

---
+++ day-18/src/main.rs
@@ -0,0 +1,86 @@
+
+#[derive(Debug)]
+enum Token {
+    Number(u64),
+    MultiplicationSymbol,
+    AdditionSymbol,
+    ExpressionStart,
+    ExpressionEnd
+}
+
+fn tokenize(input: &str) -> Vec<Token> {
+    input.chars().filter(|c| !c.is_whitespace()).map(|c| match c {
+        '(' => Token::ExpressionStart,
+        ')' => Token::ExpressionEnd,
+        '+' => Token::AdditionSymbol,
+        '*' => Token::MultiplicationSymbol,
+        n => Token::Number(n.to_digit(10).expect("could not tokenize number") as u64)
+    }).collect()
+}
+
+fn apply(accumulator: u64, operand: u64, operator: Option<Token>) -> u64 {
+    match operator {
+        Some(Token::MultiplicationSymbol) => accumulator * operand,
+        Some(Token::AdditionSymbol) => accumulator + operand,
+        _ => panic!()
+    }
+}
+
+fn evaluate(tokens: &[Token]) -> (usize, u64) {
+    let mut total = 0;
+    let mut index = 0;
+    let mut context = Some(Token::AdditionSymbol);
+
+    while index < tokens.len() {
+        let token = &tokens[index];
+
+        match token {
+            // If mult/add: set context
+            Token::AdditionSymbol => context = Some(Token::AdditionSymbol),
+            Token::MultiplicationSymbol => context = Some(Token::MultiplicationSymbol),
+            Token::Number(n) => {
+                total = apply(total, *n, context);
+                context = None;
+            }
+            Token::ExpressionStart => {
+                let (increment, n) = evaluate(&tokens[index + 1..]);
+                total = apply(total, n, context);
+                context = None;
+                index += increment;
+            }
+            Token::ExpressionEnd => return (index + 1, total)
+        }
+
+        index += 1;
+    }
+
+    (index, total)
+}
+
+fn main() {
+    // Part One
+    let sum: u64 = std::fs::read_to_string("input.txt").unwrap().lines()
+        .map(|line| {
+            let tokens = tokenize(line);
+            evaluate(&tokens).1
+        })
+        .sum();
+
+    println!("Part one: {}", sum);
+}
+
+#[test]
+fn test_tokenize() {
+    let tokens = tokenize("2 * 3 + (4 * 5)");
+    assert_eq!(evaluate(&tokens).1, 26);
+
+    let tokens = tokenize("5 + (8 * 3 + 9 + 3 * 4 * 3)");
+    assert_eq!(evaluate(&tokens).1, 437);
+
+    let tokens = tokenize("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))");
+    assert_eq!(evaluate(&tokens).1, 12240);
+
+    println!("last one");
+    let tokens = tokenize("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2");
+    assert_eq!(evaluate(&tokens).1, 13632);
+}
